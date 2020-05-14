0f81362c784d116351b03fe4d5bab3c4f1ff2cf9
Alok kumar <devopsalok@gmail.com>
2020-05-15 03:57:29 +0530

    Components Folder and files

--- aapkidava/src/App.js
+++ aapkidava/src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
+import UseAnimations from 'react-useanimations';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h2>AppKiDava </h2>
-
+        <h5>AppKiDava </h5>
+         <UseAnimations animationKey="heart"/>
       </header>
     </div>
+
   );
 }
 
